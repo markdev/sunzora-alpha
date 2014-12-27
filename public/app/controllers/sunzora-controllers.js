8a5c276074d07d6270ed12ae53d9852d01fa82eb
root <root@li60-94.members.linode.com>
2014-12-27 00:44:35 -0500

    adds controllers and routes

--- public/app/controllers/sunzora-controllers.js
+++ public/app/controllers/sunzora-controllers.js
@@ -3,6 +3,38 @@ console.log("loaded: sunzora controllers");
 angular
 	.module('sun')
 
-	.controller('SunzoraCtrl', ['$scope', function($scope){
-		console.log('SunzoraCtrl loaded...');
+	.controller('SunzoraCtrl', ['$scope', function($scope) {
+		console.log('SunzoraCtrl loaded');
+	}])
+
+	.controller('LoginCtrl', ['$scope', function($scope) {
+		console.log('LoginCtrl loaded');
+	}])
+
+	.controller('LogoutCtrl', ['$scope', function($scope) {
+		console.log('LogoutCtrl loaded');
+	}])
+
+	.controller('ContestDetailsCtrl', ['$scope', function($scope) {
+		console.log('ContestDetailsCtrl loaded');
+	}])
+
+	.controller('ContestCreateCtrl', ['$scope', function($scope) {
+		console.log('ContestCreateCtrl loaded');
+	}])
+
+	.controller('ContestAddEntryCtrl', ['$scope', function($scope) {
+		console.log('ContestAddEntryCtrl loaded');
+	}])
+
+	.controller('ContestJudgeCtrl', ['$scope', function($scope) {
+		console.log('ContestJudgeCtrl loaded');
+	}])
+
+	.controller('ContestResultsCtrl', ['$scope', function($scope) {
+		console.log('ContestResultsCtrl loaded');
+	}])
+
+	.controller('AboutCtrl', ['$scope', function($scope) {
+		console.log('AboutCtrl loaded');
 	}])
--- public/app/ui-routes/sunzora-routes.js
+++ public/app/ui-routes/sunzora-routes.js
@@ -18,17 +18,70 @@ angular
 				  abstract: true
 				, url: '/'
 				, templateUrl: '/views/default'
-				, controller: function($scope){
-					$scope.title = 'home';
-				}
 			})
 
-			.state('root.home', {
-				  url: ''
-				, parent: 'root' // any problems here?
-				, templateUrl: '/views/home/main'
-				, controller: 'SunzoraHomepageCtrl'
+			// log in
+			.state('login', {
+				  url: '/login'
+				, templateUrl: '/views/login'
+				, controller: 'LoginCtrl'
 			})
+
+			// log out
+			.state('logout', {
+				  url: '/logout'
+				, controller: 'LogoutCtrl'
+			})
+
+			// contests
+			.state('contests', {
+				  abstract: true
+				, url: '/contest'
+				, templateUrl: '/views/contestList'
+			})
+
+			// contest details
+			.state('contests.details', {
+				  url: '/:id'
+				, templateUrl: '/views/contestDetails'
+				, controller: 'ContestDetailsCtrl'
+			})
+
+			// create new contest
+			.state('contests.create', {
+				  url: '/create'
+				, templateUrl: '/views/createContest'
+				, controller: 'ContestCreateCtrl'
+			})
+
+			// add entry
+			.state('contests.addEntry', {
+				  url: '/addEntry/:id'
+				, templateUrl: '/views/addEntry'
+				, controller: 'ContestAddEntryCtrl'
+			})
+
+			// judge contests
+			.state('contests.judge', {
+				  url: '/judge/:id'
+				, templateUrl: '/views/judgeContest'
+				, controller: 'ContestJudgeCtrl'
+			})
+
+			// contest results
+			.state('contests.results', {
+				  url: '/results/:id'
+				, templateUrl: '/views/contestResults'
+				, controller: 'ContestResultsCtrl'
+			})
+
+			// about
+			.state('about', {
+				  url: '/about'
+				, templateUrl: '/views/about'
+				, controller: 'AboutCtrl'
+			})
+
 
 	// end state config
 	})
