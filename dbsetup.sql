2f5b803901f4e15a29ddcfe91986534a1ca8bccc
tlebeda <tlebeda@gmail.com>
2014-12-30 22:03:41 -0500

    updated the create table script for localization incompatibility

--- dbsetup.sql
+++ dbsetup.sql
@@ -4,8 +4,6 @@ CREATE DATABASE sunzora
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'C'
-       LC_CTYPE = 'C'
        CONNECTION LIMIT = -1;
 
 CREATE TABLE contest
