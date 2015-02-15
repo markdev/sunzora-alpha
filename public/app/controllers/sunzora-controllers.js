a6138e34d69e2c6827accdb2bda75c65a9b1f3b5
root <root@li60-94.members.linode.com>
2015-02-14 20:58:35 -0500

    adds some stuff:

--- public/app/controllers/sunzora-controllers.js
+++ public/app/controllers/sunzora-controllers.js
@@ -15,107 +15,108 @@ angular
 		$scope.email = "";
 		$scope.password = "";
 
-	        // Define user empty data :/
-	      	$scope.user = {};
-	      
-	        // Defining user logged status
-	        $scope.logged = false;
-	      
-	        // And some fancy flags to display messages upon user status change
-	        $scope.byebye = false;
-	        $scope.salutation = false;
+		// Define user empty data :/
+		$scope.user = {};
+	  
+		// Defining user logged status
+		$scope.logged = false;
+	  
+		// And some fancy flags to display messages upon user status change
+		$scope.byebye = false;
+		$scope.salutation = false;
 
-	        /**
-	       	 * Watch for Facebook to be ready.
-	         * There's also the event that could be used
-	         */
-	        $scope.$watch(
+		/**
+		 * Watch for Facebook to be ready.
+		 * There's also the event that could be used
+		 */
+		$scope.$watch(
 			function() {
-		  		return Facebook.isReady();
+				return Facebook.isReady();
 			},
 			function(newVal) {
-		  		if (newVal)
-		    		$scope.facebookReady = true;
+				if (newVal)
+					$scope.facebookReady = true;
 			}
-	    );
-	      
-	    var userIsConnected = false;
-	      
-	    Facebook.getLoginStatus(function(response) {
-			if (response.status == 'connected') {
-				userIsConnected = true;
-			}
-	    });
-	      
-			/**
-	         * IntentLogin
-	         */
-	    $scope.IntentLogin = function() {
-			if(!userIsConnected) {
-				$scope.login();
-			}
-	    };
-	      
-			/**
-			 * Login
-			 */
-	    $scope.login = function() {
-			Facebook.login(function(response) {
+		);
+	  
+		var userIsConnected = false;
+			  
+			Facebook.getLoginStatus(function(response) {
 				if (response.status == 'connected') {
-					$scope.logged = true;
-					$scope.me();
+					userIsConnected = true;
 				}
 			});
-	    };
+			  
+				/**
+				 * IntentLogin
+	         */
+			$scope.IntentLogin = function() {
+				if(!userIsConnected) {
+					$scope.login();
+				}
+			};
+			  
+				/**
+				 * Login
+				 */
+			$scope.login = function() {
+				Facebook.login(function(response) {
+					if (response.status == 'connected') {
+						console.log("loggging in");
+						$scope.logged = true;
+						$scope.me();
+					}
+				});
+			};
 	       
-	       /**
+		   /**
 			* me 
 			*/
-		$scope.me = function() {
-			Facebook.api('/me', function(response) {
-		    /**
-		     * Using $scope.$apply since this happens outside angular framework.
-		     */
-				$scope.$apply(function() {
-					$scope.user = response;
-				});  
-			});
-		};
+			$scope.me = function() {
+				Facebook.api('/me', function(response) {
+				/**
+				 * Using $scope.$apply since this happens outside angular framework.
+				 */
+					$scope.$apply(function() {
+						$scope.user = response;
+					});  
+				});
+			};
 	      
 			/**
 			 * Logout
 			 */
-	    $scope.logout = function() {
-			Facebook.logout(function() {
-				$scope.$apply(function() {
-					$scope.user   = {};
-					$scope.logged = false;  
+			$scope.logout = function() {
+				Facebook.logout(function() {
+					$scope.$apply(function() {
+						$scope.user   = {};
+						$scope.logged = false;  
+					});
 				});
-			});
-	    }
+			}
 	      
 	      /**
 	       * Taking approach of Events :D
 	       */
-	    $scope.$on('Facebook:statusChange', function(ev, data) {
-			console.log('Status: ', data);
-			if (data.status == 'connected') {
-				$scope.$apply(function() {
-					$scope.salutation = true;
-					$scope.byebye     = false;    
+			$scope.$on('Facebook:statusChange', function(ev, data) {
+				console.log('Status: ', data);
+				if (data.status == 'connected') {
+					$scope.$apply(function() {
+						$scope.salutation = true;
+						$scope.byebye     = false;    
+					});
+				} else {
+					$scope.$apply(function() {
+					$scope.salutation = false;
+					$scope.byebye     = true;
+				
+					// Dismiss byebye message after two seconds
+					$timeout(function() {
+						$scope.byebye = false;
+					}, 2000)
 				});
-			} else {
-				$scope.$apply(function() {
-				$scope.salutation = false;
-				$scope.byebye     = true;
-		    
-				// Dismiss byebye message after two seconds
-				$timeout(function() {
-					$scope.byebye = false;
-				}, 2000)
-			});
-		}
-	});
+			}
+		});
 
 
 		$scope.submit = function() {
